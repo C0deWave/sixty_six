32d639d66c3927369afacb3361c4716db6aef18d
CodeWave <jamg123123@naver.com>
2021-09-09 21:11:52 +0900

    피드 화면 개선: Refresh 인디케이터 loading 인디케이터 추가

--- lib/Screen/main_peed_page.dart
+++ lib/Screen/main_peed_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sixty_six/constant.dart';
 import '../Widget/peed_item_widget.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 //네비게이션 1번 메인 피드페이지 입니다.
 
@@ -12,6 +13,60 @@ class MainPeedPage extends StatefulWidget {
 }
 
 class _MainPeedPageState extends State<MainPeedPage> {
+  RefreshController _refreshController = RefreshController();
+  int loadingcount = 1;
+  bool isEnableBottomIndicator = true;
+  var peedList = [
+    PeedItemWidget(
+      imageurl:
+          'https://img.insight.co.kr/static/2020/08/14/700/9x958897t7a9ktzei33h.jpg',
+      userName: '훈이',
+      content: '나는 오늘도 게임을 한다.',
+      isClickThumsUp: false,
+      likeNum: 100,
+    ),
+    PeedItemWidget(
+      imageurl:
+          'https://img.insight.co.kr/static/2020/08/14/700/9x958897t7a9ktzei33h.jpg',
+      userName: '훈이',
+      content: '나는 오늘도 게임을 한다.',
+      isClickThumsUp: false,
+      likeNum: 100,
+    ),
+    PeedItemWidget(
+      imageurl:
+          'https://img.insight.co.kr/static/2020/08/14/700/9x958897t7a9ktzei33h.jpg',
+      userName: '훈이',
+      content: '나는 오늘도 게임을 한다.',
+      isClickThumsUp: false,
+      likeNum: 100,
+    ),
+    PeedItemWidget(
+      imageurl:
+          'https://img.insight.co.kr/static/2020/08/14/700/9x958897t7a9ktzei33h.jpg',
+      userName: '훈이',
+      content: '나는 오늘도 게임을 한다.',
+      isClickThumsUp: false,
+      likeNum: 100,
+    ),
+    PeedItemWidget(
+      imageurl:
+          'https://img.insight.co.kr/static/2020/08/14/700/9x958897t7a9ktzei33h.jpg',
+      userName: '훈이',
+      content: '나는 오늘도 게임을 한다.',
+      isClickThumsUp: false,
+      likeNum: 100,
+    ),
+    PeedItemWidget(
+      imageurl:
+          'https://img.insight.co.kr/static/2020/08/14/700/9x958897t7a9ktzei33h.jpg',
+      userName: '훈이',
+      content: '나는 오늘도 게임을 한다.',
+      isClickThumsUp: false,
+      likeNum: 100,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -29,18 +84,57 @@ class _MainPeedPageState extends State<MainPeedPage> {
           // 구분선
           K_DivLine,
           Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  PeedItemWidget(
+            child: SmartRefresher(
+              header: ClassicHeader(),
+              footer: ClassicFooter(),
+              enablePullUp: isEnableBottomIndicator,
+              enablePullDown: true,
+              onRefresh: () async {
+                //TODO:새로고침 기능이다. 글 불러오기
+                await Future.delayed(const Duration(milliseconds: 2000));
+                print('Refresh호출');
+                setState(() {
+                  isEnableBottomIndicator = true;
+                  loadingcount = 1;
+                  peedList.clear();
+                  peedList.add(PeedItemWidget(
                     imageurl:
                         'https://img.insight.co.kr/static/2020/08/14/700/9x958897t7a9ktzei33h.jpg',
                     userName: '훈이',
                     content: '나는 오늘도 게임을 한다.',
                     isClickThumsUp: false,
                     likeNum: 100,
-                  ),
-                ],
+                  ));
+                });
+                _refreshController.refreshCompleted();
+              },
+              onLoading: () async {
+                //TODO: 하단 로딩 기능 -> 글 추가하기
+                await Future.delayed(const Duration(milliseconds: 2000));
+                print('Loading호출');
+                setState(() {
+                  peedList.add(PeedItemWidget(
+                    imageurl:
+                        'https://img.insight.co.kr/static/2020/08/14/700/9x958897t7a9ktzei33h.jpg',
+                    userName: '훈이',
+                    content: '나는 오늘도 게임을 한다.',
+                    isClickThumsUp: false,
+                    likeNum: 100,
+                  ));
+                  loadingcount++;
+                  if (loadingcount >= 12) {
+                    //sss
+                    print('하단 인디케이터 막힘');
+                    isEnableBottomIndicator = false;
+                  }
+                });
+                _refreshController.loadComplete();
+              },
+              controller: _refreshController,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: peedList,
+                ),
               ),
             ),
           )
